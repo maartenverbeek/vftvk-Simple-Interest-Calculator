67ed35adcaec564ed4b5ca2bcd2c7f1082ab24bc
Maarten Verbeek <mp.bekie@gmail.com>
2021-02-13 13:21:07 +0000

    just another commit

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 function computeResult()
 {
-    alert("howdy");
     var principal = document.getElementById("principal").value;
-    alert(principal);
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
-    var futureyear = years + new Date().getFullYear();
+    var futureyear = parseInt(years) + new Date().getFullYear();
     var result = document.getElementById("result");
     result.innerHTML = 
-        <div>
+        '<div>' +
             'If you deposit <span class="number" id="principalResult\">' + principal + '</span>,<br/>' +
             'at an interest rate of <span class="number" id="rateResult">' + rate + '</span>%.<br/>' +
             'You will receive an amount of <span class="number" id="interestResult">' + interest + '</span>,<br/>' +
-            'in the year <span class="number" id="futureYearResult">' + futureyear + '</span>.<br/>';
-        </div>
+            'in the year <span class="number" id="futureYearResult">' + futureyear + '</span>.<br/>' +
+        '</div>'
 }
 
 function displayRate()
 {
     var rate = document.getElementById("rate").value;
     var rateValue = document.getElementById("rateValue");
-    rateValue.innerText = rate + " %";
+    rateValue.textContent = rate + " %";
 }
 
 function validatePrincipal()
